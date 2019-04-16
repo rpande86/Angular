4a67716d45d434535119ebd122c4636ee78a7d7f
rpande86 <pandeyroshan15@gmail.com>
2019-04-16 18:39:05 +0530

    except node module, app and asset

---
+++ ircmsNew/IrcmsNew/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  authURL:'http://10.26.24.36:8073/'
+};
---
+++ ircmsNew/IrcmsNew/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  authURL:'http://10.26.24.36:8073/',
+  apiMasterURL : 'http://lt0055302:9000/'
+};
